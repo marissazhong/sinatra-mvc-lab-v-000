e912e03192a2fc00b9c9347f3fe6462aaf01e1e9
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 21:23:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,15 +4,15 @@ class PigLatinizer
     # @text = text
   end
 
-  def piglatinize
-    if "aeiou".include?(@text[0])
-      result = "#{@text[1..-1]+@text[0]}ay"
+  def piglatinize(text)
+    if "aeiou".include?(text[0])
+      result = "#{text[1..-1]+text[0]}ay"
     else
       i=0
-      while !"aeiou".include?(@text[i])
+      while !"aeiou".include?(text[i])
         i+=1
       end
-      result = "#{@text[i..-1]+@text[0..i-1]}ay"
+      result = "#{text[i..-1]+text[0..i-1]}ay"
     end
     result
   end
