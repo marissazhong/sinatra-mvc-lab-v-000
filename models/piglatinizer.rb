8fbcd374630656426483f4b898853c2dfec33da9
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 21:17:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,14 +6,14 @@ class PigLatinizer
 
   def latinize
     if "aeiou".include?(@text[0])
-      @result = "#{text[1..-1]+text[0]}ay"
+      result = "#{text[1..-1]+text[0]}ay"
     else
       i=0
       while !"aeiou".include?(@text[i])
         i+=1
       end
-      @result = "#{text[i..-1]+text[0..i-1]}ay"
+      result = "#{text[i..-1]+text[0..i-1]}ay"
     end
-    @result
+    result
   end
 end
