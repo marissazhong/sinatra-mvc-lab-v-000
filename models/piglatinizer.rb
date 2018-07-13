7a3e2b64e91b30e87e1bb01c9823e6f0e23b5521
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 21:25:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
       result = "#{text[1..-1]+text[0]}way"
     else
       i=0
-      while !"aeiou".include?(text[i])
+      while !"aeiouAEIOU".include?(text[i])
         i+=1
       end
       result = "#{text[i..-1]+text[0..i-1]}ay"
